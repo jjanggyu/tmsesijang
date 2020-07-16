ee0a23531f736b5cbe9c3ddfa8a0215dc1d05bfd
BeJ Park <bjpark0506@naver.com>
2020-07-16 18:36:03 +0900

    처음 인트로 화면 추가

---
+++ tmsesijang/app/src/main/java/com/cau/a20160392/SlpashActivity.java
@@ -0,0 +1,24 @@
+package com.cau.a20160392;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SlpashActivity extends AppCompatActivity {
+
+    Handler handler = new Handler();
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+            }
+        },1000);
+    }
+}
